9824b909dd1c80296b52048374ae6c50744fc35d
TimothyAnggara <timothyanggara@gmail.com>
2023-08-26 14:51:40 +1000

    Added Weekly, Monthly, and Daily fetches

--- fetchAPI.py
+++ fetchAPI.py
@@ -4,11 +4,24 @@ import json
 API_KEY = "GBEXJPOTUHCC9FZC" #Alpha Vantage
 
 
-def fetchData(SYMBOL):
-    API_URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={SYMBOL}&apikey={API_KEY}"
+def fetchDataMonthly(SYMBOL):
+    API_URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={SYMBOL}&apikey={API_KEY}"
     r = requests.get(API_URL).json()
     print(r)
     return r
+
+def fetchDataWeekly(SYMBOL):
+    API_URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={SYMBOL}&apikey={API_KEY}"
+    r = requests.get(API_URL).json()
+    print(r)
+    return r
+
+def fetchDataDaily(SYMBOL):
+    API_URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={SYMBOL}&apikey={API_KEY}"
+    r = requests.get(API_URL).json()
+    print(r)
+    return r
+
 
 r = fetchData("AAPL")
 
